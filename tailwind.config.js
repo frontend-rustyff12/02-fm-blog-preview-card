36c9c91dcb4f62f6ad81d3f49c430bc482d67167
Russell <75209707+rustyff12@users.noreply.github.com>
2025-08-13 00:33:27 +0200

    start of styling

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// module.exports = {
+//   content: ["./**/*.html", "./src/**/*.{js}"],
+//   theme: {
+//     extend: {
+//       fontFamily: {
+//         figtree: ["Figtree", "sans-serif"],
+//       },
+//       colors: {
+//         customYellow: "hsl(47, 88%, 63%)",
+//       },
+//     },
+//   },
+// };
